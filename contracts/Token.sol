73d5d39fe3a8f3c6a2376d9f11dd0b3908dd645b
Denis LAMARD <lamard.denis@gmail.com>
2020-09-27 19:31:56 +0200

    add build front client

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma experimental ABIEncoderV2;
 pragma solidity >=0.4.22 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -18,9 +19,33 @@ library Utils {
 
 contract Token is ERC721, Ownable {
 
+    struct PetInfo {
+        string first_name;
+        string last_name;
+        string email;
+        string name_pet;
+        string type_pet;
+        string birthdate_pet;
+        string photo_hash;
+    }
+
+    /*
+                firstname: null,
+            lastname: null,
+            email: null,
+            petname: null,
+            petcolor: null,
+            pettype: null,
+            petbirthdate: null,
+            photo_hash: null,
+            terms: false
+    */
+
     //using Utils for string;
     using Counters for Counters.Counter;
     using Strings for uint256;
+
+    mapping (uint256 => PetInfo) private _list_pets;
 
     Counters.Counter private _petIds;
 
@@ -29,11 +54,13 @@ contract Token is ERC721, Ownable {
         _setBaseURI(baseURI);
     }
 
-    function addPet(address owner) public payable returns (uint256 Id) {
+    function addPet(address owner, PetInfo memory info) public payable returns (uint256 Id) {
         _petIds.increment();
         Id = _petIds.current();
         _mint(owner, Id);
         _setTokenURI(Id, Id.toString());
+        _list_pets[Id] = info;
+
         return Id;
     }
 
