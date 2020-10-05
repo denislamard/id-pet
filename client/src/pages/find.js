57c405b1ec53c703446b2c3716f87a5091ce9b60
Denis LAMARD <lamard.denis@gmail.com>
2020-10-05 07:37:05 +0200

    changes

--- client/src/pages/find.js
+++ client/src/pages/find.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from "react";
+import {MDBBtn, MDBCol, MDBContainer, MDBIcon, MDBInput, MDBRow} from 'mdbreact';
 import {BasePage} from './base';
 
 class FindPage extends BasePage {
@@ -6,8 +7,21 @@ class FindPage extends BasePage {
     render() {
         return (
             <Fragment>
-                <h1>Find a Pet </h1>
-                <p>{this.state.account}</p>
+                <MDBContainer>
+                    <h2 className="indigo-text font-weight-bold mt-2 mb-2"><MDBIcon icon="search"/> Find a pet</h2>
+                    <MDBRow center>
+                        <MDBCol sm="6">
+                            <div className={"create-div"}>
+                                <MDBInput label="Type a valid ID" icon="user" group type="text"/>
+                            </div>
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBRow center>
+                        <MDBCol sm="6" className="text-right">
+                            <MDBBtn outline color="success" onClick={this.handleClose}>Find</MDBBtn>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
             </Fragment>
         );
     }
