783e90f66d33e6142c654cbafd8dd233a282fbe3
Denis LAMARD <lamard.denis@gmail.com>
2020-10-06 20:46:16 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c0f9759740682302492ec13fb1d6d68.js"
+  "/precache-manifest.eac237fbf13a07b63a8f87b11379ea8e.js"
 );
 
 self.addEventListener('message', (event) => {
